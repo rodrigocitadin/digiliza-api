c820b7317d8c57e9b1b555ba83111f786b63ea0c
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-22 16:28:38 -0300

    user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { Role } from "@prisma/client";
+import { IsString, IsNotEmpty, IsEnum, IsOptional, IsStrongPassword, IsEmail } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEnum(Role) 
+  @IsOptional()
+  role?: Role;
+}
