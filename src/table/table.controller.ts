719c95121db975e39c89b9fa2c1132baf765278f
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 15:26:00 -0300

    get all or one table

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TableService } from './table.service';
 import { CreateTableDto } from './dto/create-table.dto';
 
@@ -9,6 +9,17 @@ export class TableController {
   @Post()
   async create(@Body() createTableDto: CreateTableDto) {
     const table = await this.tableService.create(createTableDto);
+    return table;
+  }
+
+  @Get()
+  async findAll() {
+    return await this.tableService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    const table = await this.tableService.findById(+id);
     return table;
   }
 }
