57f2b140d1a244145382de0804970eb1b8d4af75
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 15:24:04 -0300

    table create route

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TableService } from './table.service';
+import { CreateTableDto } from './dto/create-table.dto';
 
 @Controller('table')
 export class TableController {
   constructor(private readonly tableService: TableService) {}
+
+  @Post()
+  async create(@Body() createTableDto: CreateTableDto) {
+    const table = await this.tableService.create(createTableDto);
+    return table;
+  }
 }
