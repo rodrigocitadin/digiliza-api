4a0feacc627928af50f1100e0d71d99e29b729af
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 03:40:33 -0300

    create table service

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTableDto } from './dto/create-table.dto';
 
 @Injectable()
 export class TableService {
   constructor(private prisma: PrismaService) { }
+
+  async create(createTableDto: CreateTableDto) {
+    try {
+      const table = await this.prisma.table.create({
+        data: createTableDto
+      })
+
+      return table;
+    }
+    catch (error) {
+      throw new BadRequestException(error.name);
+    }
+  }
 }
