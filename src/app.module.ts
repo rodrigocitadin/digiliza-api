d2531b6fc0c75bb163b5e923d1bc96d96331edf9
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 03:31:16 -0300

    create table resources with nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TableModule } from './table/table.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, TableModule],
 })
 export class AppModule {}
