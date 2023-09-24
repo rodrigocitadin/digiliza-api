c647f1d87cd9cad846b59f5882a649c0a09bb7ca
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 23:17:52 -0300

    create reservation dto

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,19 @@
-export class CreateReservationDto {}
+import { IsBoolean, IsDate, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateReservationDto {
+  @IsString()
+  @IsNotEmpty()
+  user_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  table_id: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  to_date: Date;
+
+  @IsBoolean()
+  @IsOptional()
+  active?: boolean;
+}
