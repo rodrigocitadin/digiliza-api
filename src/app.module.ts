cd8c34d717817599a78d2c490f8e32c5bb373c0e
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-23 23:04:00 -0300

    generate reservation service and controller with nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TableModule } from './table/table.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, TableModule],
+  imports: [UserModule, PrismaModule, TableModule, ReservationModule],
 })
 export class AppModule {}
