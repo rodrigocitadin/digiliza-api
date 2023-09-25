7491fb94cab810e6bd75c7cf799eaad1b715aebc
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-25 14:06:27 -0300

    finished module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,19 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt/dist';
-import { jwtConstants } from './constants';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './constants';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UserModule,
+    PassportModule,
     JwtModule.register({
-      global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: "7d" }
     })
-  ]
+  ],
+  exports: [AuthService]
 })
 export class AuthModule { }
