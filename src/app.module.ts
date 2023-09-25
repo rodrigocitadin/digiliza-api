91c2ab8fc0bb94353882861425c064e6534d87f6
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-25 12:58:11 -0300

    add nest jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TableModule } from './table/table.module';
 import { ReservationModule } from './reservation/reservation.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, TableModule, ReservationModule],
+  imports: [UserModule, PrismaModule, TableModule, ReservationModule, AuthModule],
 })
 export class AppModule {}
