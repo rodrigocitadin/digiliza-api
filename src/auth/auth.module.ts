428051ee21cb2a65c4e41fe64c37cc4afc50d04a
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-25 13:04:45 -0300

    create constants and jwt service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { jwtConstants } from './constants';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    PrismaService,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "7d" }
+    })
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
