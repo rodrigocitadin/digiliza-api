283c4cf1379a8528bf189721c9f54455253afc09
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-25 13:53:55 -0300

    compare passwords and return user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) { }
+
+  async validateUser(email: string, password: string): Promise<Omit<User, "password"> | null> {
+    const user = await this.userService.findToLogin(email);
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (user && matchPassword) {
+      const { password, ...result } = user;
+
+      return result;
+    }
+
+    return null;
+  }
+}
