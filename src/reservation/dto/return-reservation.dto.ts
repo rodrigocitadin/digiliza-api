c6c7bd1cab4474b8b88d3fe6b978a3b8f2bd964b
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-16 18:34:07 -0300

    switch reservation return query to a isolated file

--- src/reservation/dto/return-reservation.dto.ts
+++ src/reservation/dto/return-reservation.dto.ts
@@ -1,3 +1,11 @@
 import { Reservation } from "@prisma/client";
 
 export type ReturnReservationDto = Pick<Reservation, "id" | "user_id" | "table_id" | "active" | "date">
+
+export const returnReservationQuery = {
+  id: true,
+  user_id: true,
+  table_id: true,
+  active: true,
+  date: true
+}
